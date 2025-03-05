84dd076c250268de489e600061bcd20041439330
남진영 <91328640@ktfriend.com>
2025-03-05 13:15:53 +0900

    1.0 -1 버전에 맞추고 해결방법 보면서 run 되도록 수정

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./app/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
+
