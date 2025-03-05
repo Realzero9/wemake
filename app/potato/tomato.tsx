96bf7480cfde1ed40eaee51885433d881ebd2949
남진영 <91328640@ktfriend.com>
2025-03-05 17:03:56 +0900

    1.3 Layout Component

--- app/potato/tomato.tsx
+++ app/potato/tomato.tsx
@@ -1,4 +1,13 @@
 export default function AboutUs() {
-  return <div>Tomato</div>;
+    return (
+        <div className="tomato-container">
+            <h1 className="tomato-title">Tomato</h1>
+        </div>
+    );
 }
 
+export const links = () => [{ rel: "stylesheet", href: "/app/potato/tomato.css" }];
+export const meta = () => [{
+    title: "Tomato",
+    description: "Tomato is a tomato"
+}];
