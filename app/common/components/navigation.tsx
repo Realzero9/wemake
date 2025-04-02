beb10e23f8a5aefed543788bb3cbb93f2d57ffcc
남진영 <91328640@ktfriend.com>
2025-04-02 12:45:13 +0900

    #3.2 Navigation part Two

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import { Separator } from "./ui/separator";
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { cn } from "~/lib/utils";
 
 const menus = [
     {
@@ -126,14 +127,40 @@ export function Navigation() {
             <NavigationMenuList>
                 {menus.map(menu => 
                 <NavigationMenuItem key={menu.name}>
-                    <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        {menu.items?.map(item => (
-                            <NavigationMenuLink key={item.label}>
-                                <Link to={item.to}>{item.label}</Link>
-                            </NavigationMenuLink>
-                        ))}
+                    {menu.items?
+                        <>
+                            <Link to={menu.to}>
+                                <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                            </Link>
+                            <NavigationMenuContent>
+                        <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                            {menu.items?.map(item => (
+                                <NavigationMenuItem key={item.name} 
+                                    className={cn([
+                                        "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                                        item.to === "/products/promote" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                        item.to === "/jobs/submit" && "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                                    ])}
+                                >
+                                    <NavigationMenuLink asChild>
+                                        <Link
+                                            className="p-3 space-y-1 block leading-none no-underline outline-none"
+                                            to={item.to}
+                                        >
+                                            <span className="text-sm font-medium leading-none">{item.name}</span>
+                                            <p className="text-sm leading-snug text-muted-foreground">
+                                                {item.description}
+                                            </p>
+                                        </Link>
+                                    </NavigationMenuLink>
+                                </NavigationMenuItem>
+                            ))}
+                        </ul>
                     </NavigationMenuContent>
+                    </>
+                    :
+                    <Link className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 data-[state=open]:hover:bg-accent data-[state=open]:focus:bg-accent" to={menu.to}>{menu.name}</Link>
+                    }
                 </NavigationMenuItem>
             )}
             </NavigationMenuList>
