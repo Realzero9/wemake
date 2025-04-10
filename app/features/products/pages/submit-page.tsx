69085402ae39eaf0265e07365bc75692773d606e
남진영 <91328640@ktfriend.com>
2025-04-10 16:48:02 +0900

    #3.23 Submit Product Page

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -4,6 +4,9 @@ import type { Route } from "./+types/submit-page";
 import { Form } from "react-router";
 import InputPair from "~/common/components/input-pair";
 import SelectPair from "~/common/components/select-pair";
+import { Input } from "~/common/components/ui/input";
+import { Label } from "~/common/components/ui/label";
+import { useState } from "react";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -13,6 +16,13 @@ export const meta: Route.MetaFunction = () => {
 }
 
 export default function SubmitPage() {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setIcon(URL.createObjectURL(file));
+    }
+  }
   return (
     <div>
       <Hero title="Submit Your Product" subtitle="Share your product with the world" />
@@ -65,6 +75,33 @@ export default function SubmitPage() {
               { label: "Category 2", value: "category-2" },
             ]}
           />
+          <Button type="submit" className="w-full" size="lg">Submit</Button>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <div className="size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ?
+              <img src={icon} alt="Icon" className="w-full h-full object-cover" />
+              : null
+            }
+          </div>
+          <Label className="flex flex-col gap-1">
+            Icon
+            <small className="text-muted-foreground">
+              This is the icon of your product.
+            </small>
+          </Label>
+          <Input type="file" className="w-1/2" onChange={onChange} required name="icon" />
+          <div className="flex flex-col">
+            <span className="text-muted-foreground">
+              Recommended size: 128x128px
+            </span>
+            <span className="text-muted-foreground">
+              Allowed formats: PNG, JPEG
+            </span>
+            <span className="text-muted-foreground">
+              Max file size: 1MB
+            </span>
+          </div>
         </div>
       </Form>
     </div>
