3608f0acdd2f05ee876d879fa0e0d040430befb2
남진영 <91328640@ktfriend.com>
2025-04-24 13:16:15 +0900

    #4.1 Drizzle Setup
    - npm i drizzle-orm postgres --legacy-peer-deps
    - npm i -D drizzle-kit --legacy-peer-deps

---
+++ app/db.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+// connection pool - drizzle은 지원하지 않음
+const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+
+const db = drizzle(client);
+
+export default db;
---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+// drizzel kit 이 정보 확인할때 참조하는 파일
+export default defineConfig({
+    schema: "./app/features/**/schema.ts",
+    out: "./app/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
