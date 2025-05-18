458943cf8da6f0c6a45264e01087130acbc2a2e7
남진영 <91328640@ktfriend.com>
2025-05-18 20:09:02 +0900

    #8.2 Create Team Page
    - 페이지 들어왔을때 GET 요청 보호 != 데이터 로드하는 POST 요청 보호 (각각 따로 client 호출하여 사용자인증 거쳐야함)
    - formData 데이터를 보낼때 number는 사실 string으로 전달되어서 zod 설정에 coerce 를 넣어줘야함

--- app/features/teams/pages/submit-team-page.tsx
+++ app/features/teams/pages/submit-team-page.tsx
@@ -1,10 +1,14 @@
-import { Form } from "react-router";
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/submit-team-page";
 import { Hero } from "~/common/components/hero";
 import { Button } from "~/common/components/ui/button";
 import InputPair from "~/common/components/input-pair";
 import SelectPair from "~/common/components/select-pair";
 import { PRODUCT_STAGES } from "../constants";
+import { getLoggedInUserId } from "~/features/users/queries";
+import { makeSSRClient } from "~/supa-client";
+import { z } from "zod";
+import { createTeam } from "../mutation";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -12,11 +16,39 @@ export const meta: Route.MetaFunction = () => {
   ];
 }
 
-export default function SubmitTeamPage() {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const { client } = makeSSRClient(request);
+  await getLoggedInUserId(client);
+}
+
+export const formSchema = z.object({
+  name: z.string().min(1).max(20),
+  stage: z.string(),
+  size: z.coerce.number().min(1).max(100),
+  equity: z.coerce.number().min(1).max(100),
+  roles: z.string(),
+  description: z.string().min(1).max(200),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const { client } = makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  const formData = await request.formData();
+  const { success, data, error } = formSchema.safeParse(Object.fromEntries(formData));
+  if (!success) {
+    return {
+      fieldErrors: error?.flatten().fieldErrors,
+    };
+  }
+  const { team_id } = await createTeam(client, userId, {...data});
+  return redirect(`/teams/${team_id}`);
+}
+
+export default function SubmitTeamPage({ actionData }: Route.ComponentProps) {
   return (
     <div className="space-y-20">
       <Hero title="Create Team" subtitle="Create a team to find a team mate" />
-      <Form className="max-w-screen-2xl flex flex-col items-center gap-10 mx-auto">
+      <Form className="max-w-screen-2xl flex flex-col items-center gap-10 mx-auto" method="post">
         <div className="grid grid-cols-3 gap-10 w-full">
             <InputPair
                 label="What is the name of the product?"
@@ -28,6 +60,13 @@ export default function SubmitTeamPage() {
                 id="name"
                 required
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.name?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
             <SelectPair
                 label="What is the stage of your product?"
                 description="Select the stage of your product"
@@ -36,6 +75,13 @@ export default function SubmitTeamPage() {
                 placeholder="Select the stage of your product"
                 options={PRODUCT_STAGES}
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.stage?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
             <InputPair
                 label="What is the size of your team?"
                 description="(1-100)"
@@ -46,6 +92,13 @@ export default function SubmitTeamPage() {
                 id="size"
                 required
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.size?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
             <InputPair
                 label="How much equity are you willing to give?"
                 description="(*each)"
@@ -56,6 +109,13 @@ export default function SubmitTeamPage() {
                 id="equity"
                 required
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.equity?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
             <InputPair
                 label="What roles are you looking for?"
                 placeholder="e.g. React Developer, Backend Developer, Product Developer"
@@ -65,6 +125,13 @@ export default function SubmitTeamPage() {
                 id="roles"
                 required
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.roles?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
             <InputPair
                 label="What is the description of your product?"
                 description="(200 characters max)"
@@ -76,6 +143,13 @@ export default function SubmitTeamPage() {
                 required
                 textArea
             />
+            { actionData && "fieldErrors" in actionData && (
+              <div className="text-red-500">
+                {actionData.fieldErrors?.description?.map((error) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            )}
         </div>
         <Button type="submit" className="w-full max-w-sm" size="lg">
             Create Team
