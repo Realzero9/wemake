ba52269d897344f150dd8cfc5a63627459a925dc
남진영 <91328640@ktfriend.com>
2025-11-26 18:23:00 +0900

    #13.2 Request Payment
    - 카카오페이를 통한 결제까지 가능함
    - window.location.orgin : 원래 주소 참조
    - window.location.href : 지금 있는곳의 주소 참조

--- app/features/products/pages/promote-page.tsx
+++ app/features/products/pages/promote-page.tsx
@@ -31,9 +31,9 @@ export default function PromotePage() {
     const widgets = useRef<TossPaymentsWidgets|null>(null);
     const initedToss = useRef<boolean>(false);
     useEffect(() => {
-      if (initedToss.current) return;
-      initedToss.current = true;
       const initToss = async () => {
+        if (initedToss.current) return;
+        initedToss.current = true;
         const toss = await loadTossPayments("test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm");
         widgets.current = await toss.widgets({
           customerKey: "1111111",
@@ -52,21 +52,44 @@ export default function PromotePage() {
       initToss();
     }, []);
     useEffect(() => {
-      if (widgets.current) {
-        widgets.current.setAmount({
-          value: totalDays * 20000,
-          currency: "KRW",
-        });
-      }
+      const updateAmount = async () => {
+        if (widgets.current) {
+          await widgets.current.setAmount({
+            value: totalDays * 20000,
+            currency: "KRW",
+          });
+        }
+      };
+      updateAmount();
     }, [promotionPeriod]);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const formData = new FormData(e.target as HTMLFormElement);
+      const product = formData.get("product") as string;
+      if (!product || !promotionPeriod?.to || !promotionPeriod?.from) return;
+      await widgets.current?.requestPayment({
+        orderId: crypto.randomUUID(),
+        orderName: `WeMake Promotion for ${product}`,
+        customerEmail: "nico@nomadcoders.co",
+        customerName: "Nico",
+        customerMobilePhone: "01012345678",
+        metadata: {
+          product,
+          promotionFrom: DateTime.fromJSDate(promotionPeriod.from).toISO(),
+          promotionTo: DateTime.fromJSDate(promotionPeriod.to).toISO(),
+        },
+        successUrl: `${window.location.href}/success`,
+        failUrl: `${window.location.href}/fail`,
+      })
+    };
   return (
     <div>
       <Hero
         title="Promote Your Product"
         subtitle="Boost your product's visibility."
       />
-      <div className="grid grid-cols-6 gap-10">
-        <Form className="col-span-3 mx-auto w-1/2 flex flex-col gap-10 items-start">
+      <form onSubmit={handleSubmit} className="grid grid-cols-6 gap-10">
+        <div className="col-span-3 mx-auto w-1/2 flex flex-col gap-10 items-start">
           <SelectPair
             label="Select a product"
             description="Select the product you want to promote."
@@ -76,14 +99,6 @@ export default function PromotePage() {
               {
                 label: "AI Dark Mode Maker",
                 value: "ai-dark-mode-maker",
-              },
-              {
-                label: "AI Dark Mode Maker2",
-                value: "ai-dark-mode-maker2",
-              },
-              {
-                label: "AI Dark Mode Maker3",
-                value: "ai-dark-mode-maker3",
               },
             ]}
           />
@@ -102,7 +117,7 @@ export default function PromotePage() {
               disabled={{ before: new Date() }}
             />
           </div>
-        </Form>
+        </div>
         <aside className="col-span-3 px-20 flex flex-col items-center">
           <div id ="toss-payment-methods" className="w-full" />
           <div id="toss-payment-agreement" />
@@ -116,7 +131,7 @@ export default function PromotePage() {
             )
           </Button>
         </aside>
-      </div>
+      </form>
     </div>
   );
 } 
