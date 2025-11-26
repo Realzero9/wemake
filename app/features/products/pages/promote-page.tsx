3fde43922e85e78fb33e1737231fe5430d1b41b9
남진영 <91328640@ktfriend.com>
2025-11-26 17:37:34 +0900

    #13.1 Payments Widgets - 재수강

--- app/features/products/pages/promote-page.tsx
+++ app/features/products/pages/promote-page.tsx
@@ -29,10 +29,13 @@ export default function PromotePage() {
         ).days
       : 0;
     const widgets = useRef<TossPaymentsWidgets|null>(null);
+    const initedToss = useRef<boolean>(false);
     useEffect(() => {
+      if (initedToss.current) return;
+      initedToss.current = true;
       const initToss = async () => {
-        const toss = loadTossPayments("test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm");
-        widgets.current = (await toss).widgets({
+        const toss = await loadTossPayments("test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm");
+        widgets.current = await toss.widgets({
           customerKey: "1111111",
         });
         await widgets.current.setAmount({
@@ -40,8 +43,7 @@ export default function PromotePage() {
           currency: "KRW"
         });
         await widgets.current.renderPaymentMethods({
-          selector: "#toss-payment-widget",
-          variantKey: "DEFAULT",
+          selector: "#toss-payment-methods",
         });
         await widgets.current.renderAgreement({
           selector: "#toss-payment-agreement",
@@ -76,16 +78,16 @@ export default function PromotePage() {
                 value: "ai-dark-mode-maker",
               },
               {
-                label: "AI Dark Mode Maker",
-                value: "ai-dark-mode-maker-1",
+                label: "AI Dark Mode Maker2",
+                value: "ai-dark-mode-maker2",
               },
               {
-                label: "AI Dark Mode Maker",
-                value: "ai-dark-mode-maker-2",
+                label: "AI Dark Mode Maker3",
+                value: "ai-dark-mode-maker3",
               },
             ]}
           />
-          <div className="flex flex-col gap-2 items-center w-full">More actions
+          <div className="flex flex-col gap-2 items-center w-full">
             <Label className="flex flex-col gap-1">
               Select a range of dates for promotion{" "}
               <small className="text-muted-foreground text-center ">
@@ -102,7 +104,7 @@ export default function PromotePage() {
           </div>
         </Form>
         <aside className="col-span-3 px-20 flex flex-col items-center">
-          <div id ="toss-payment-widget" className="w-full" />
+          <div id ="toss-payment-methods" className="w-full" />
           <div id="toss-payment-agreement" />
           <Button
             className="w-full"
@@ -110,7 +112,8 @@ export default function PromotePage() {
             Checkout ({(totalDays * 20000).toLocaleString("ko-KR", {
               style: "currency",
               currency: "KRW",
-            })})
+            })}
+            )
           </Button>
         </aside>
       </div>
