07e0c5cdc287bc7657c217f2b7f5e2abbf3080e2
남진영 <91328640@ktfriend.com>
2025-11-26 15:55:47 +0900

    @ supabase 멈춰서 복원후 재세팅 - 라이브러리 업데이트도 있었음.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
@@ -10,11 +8,6 @@ import path from "path";
 // Build System
 // 코드를 가져다가 다른것으로 변환해줌
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
   plugins: [reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
