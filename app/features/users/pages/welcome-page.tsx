a93da7e6a6b09e8539d4a7bbfcb3d9887dfda980
남진영 <91328640@ktfriend.com>
2025-11-26 17:13:16 +0900

    #12.2~4 Resend 연동 및 email 보내기
    1. npm i resend
    
    2. npx create-email@latest
    3. cd react-email-starter npm i
    4. npm run dev

---
+++ app/features/users/pages/welcome-page.tsx
@@ -0,0 +1,17 @@
+import { Resend } from "resend";
+import { render } from "@react-email/components";
+import type { Route } from "./+types/welcome-page";
+import { WelcomeUser } from "../../../../react-email-starter/emails/welcome-user";
+
+const client = new Resend(process.env.RESEND_API_KEY);
+
+export const loader = async ({params}: Route.LoaderArgs) => {
+    //const emailHtml = await render(<WelcomeUser username={params.username} />);
+    const { data, error } = await client.emails.send({
+        from : "rz <rz@mail.windinato.online>",
+        to: "ninnk9@gmail.com",
+        subject: "Welcome to windinato online the app",
+        react: <WelcomeUser username={params.username} />,
+    });
+    return Response.json({ data, error });
+};
