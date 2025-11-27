b60a5b2536bcc945286e3a30f12a3ddfc41855ab
남진영 <91328640@ktfriend.com>
2025-11-27 13:49:58 +0900

    #13.3 Confirm Payment
    - TossPay가 성공했을 경우의 화면 구성

---
+++ app/features/products/pages/promote-success-page.tsx
@@ -0,0 +1,37 @@
+import { z } from "zod";
+import type { Route } from "./+types/promote-success-page";
+
+const paramsSchema = z.object({
+    paymentType: z.string(),
+    orderId: z.string().uuid(),
+    paymentKey: z.string(),
+    amount: z.coerce.number(),
+});
+
+const secretKey = process.env.TOSS_SECRET_KEY;
+
+export const loader = async ({request}: Route.LoaderArgs) => {
+    const url = new URL(request.url);
+    const {success, data} = paramsSchema.safeParse(Object.fromEntries(url.searchParams));
+    if(!success) {
+        return new Response(null, { status: 400 });
+    }
+    const encryptedSecretKey = `Basic ${Buffer.from(secretKey + ":").toString("base64")}`;
+    const response = await fetch(
+        "https://api.tosspayments.com/v1/payments/confirm",
+        {
+            method: "POST", 
+            headers: {
+                Authorization: encryptedSecretKey, 
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify({
+                orderId: data.orderId,
+                paymentKey: data.paymentKey,
+                amount: data.amount,
+            }),
+        }
+    );
+    const responseData = await response.json();
+    return Response.json(responseData);
+};
