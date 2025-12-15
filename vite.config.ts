7caadaa232c3cb2e0fdca60c57b4c0319f459572
남진영 <91328640@ktfriend.com>
2025-12-15 10:29:51 +0900

    Revert "[추가] Sentry 모니터링 시스템 추가"
    This reverts commit 6e35964077b11358e3ae96287da8966ca9c5fb80.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
-import { reactRouter } from '@react-router/dev/vite';
-import { sentryReactRouter, type SentryReactRouterBuildOptions } from '@sentry/react-router';
-
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-
-const sentryConfig: SentryReactRouterBuildOptions = {
-  org: "windinato",
-  project: "wemake",
-  // An auth token is required for uploading source maps;
-  // store it in an environment variable to keep it secure.
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  // ...
-};
 
 // tailwindcss 설정
 // 테일윈드 설정을 추가합니다.
 // Build System
 // 코드를 가져다가 다른것으로 변환해줌
-export default defineConfig((config) => ({
-  plugins: [reactRouter(), tsconfigPaths(), sentryReactRouter(sentryConfig, config)],
+export default defineConfig({
+  plugins: [reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./app"),
@@ -31,5 +20,4 @@ export default defineConfig((config) => ({
     },
     allowedHosts: ["exams-adopt-sms-irc.trycloudflare.com"],
   },
-})
-);
+});
